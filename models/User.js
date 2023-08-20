631e9d5d15af562f3f70eb8d4ff520adbfa6a245
PythonFabi <129299589+PythonFabi@users.noreply.github.com>
2023-08-19 20:11:08 -0400

    start creating user schema

--- models/User.js
+++ models/User.js
@@ -2,6 +2,22 @@ const { Schema, model } = require('mongoose');
 
 const userSchema = new Schema(
     {
-        username: String
+        username: {
+            type: String,
+            unique: true,
+            required: true,
+            trim: true
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            validate: {
+                validator: function (value) {
+                    const emailRegex = /^[^\s@]+
+                }
+            }
+        }
+
     }
 )
