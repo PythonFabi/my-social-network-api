59e7cb012231e29876cb18840354ad49f71a1c7d
PythonFabi <129299589+PythonFabi@users.noreply.github.com>
2023-08-30 14:42:25 -0400

    add routes for users and thoughts

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,8 +1,8 @@
 const router = require('express').Router();
-const 
-const
+const thoughtRoutes = require('./thoughtRoutes');
+const userRoutes = require('./userRoutes');
 
-router.use('/', );
-router.use('/', );
+router.use('/thoughts', thoughtRoutes);
+router.use('/users', userRoutes);
 
 module.exports = router;
