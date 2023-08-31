efaa187acb23680b24549c9cbe36d0825583bf93
PythonFabi <129299589+PythonFabi@users.noreply.github.com>
2023-08-31 11:11:51 -0400

    create all userController functions

---
+++ controllers/userController.js
@@ -0,0 +1,107 @@
+const { User, Thought } = require('../models');
+
+module.exports = {
+    async getUsers(req, res) {
+        try {
+            const users = await User.find();
+            res.json(users);
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    },
+    async getSingleUser(req, res) {
+        try {
+            const user = await User.findOne({ _id: req.params.userId })
+              .select('-__v');
+
+              if(!user) {
+                return res.status(404).json({ message: 'No user with that ID' });
+              }
+
+              res.json(user);
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    },
+    async createUser(req, res) {
+        try {
+            const dbUserData = await User.create(req.body);
+            res.json(dbUserData);
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    },
+    async updateUser(req, res) {
+        try {
+            const user = await User.findOneAndUpdate(
+                { _id: req.params.userId },
+                { $set: req.body },
+                { runValidators: true, new: true }
+            );
+
+            if(!user) {
+                return res.status(404).json({ message: 'No user with that ID' });
+            }
+
+            res.json(user);
+        } catch (err) {
+            console.log(err);
+            res.status(500).json(err);
+        }
+    },
+    async deleteUser(req, res) {
+        try {
+            const user = await User.findOneAndDelete({ _id: req.params.userId });
+
+            if (!user) {
+                return res.status(404).json({ message: 'No user with that ID' });
+            }
+
+            await Thought.deleteMany({ _id: { $in: user.thoughts }});
+            res.json({ message: 'User and associated thoughts deleted!' });
+        } catch (err) {
+            console.log(err);
+            res.status(500).json(err);
+        }
+    },
+    async addFriend(req, res) {
+        try {
+            const user = await User.findOneAndUpdate(
+                { _id: req.params.userId },
+                { $addToSet: { friends: req.body }},
+                { runValidators: true, new: true }
+            );
+            
+            if(!user) {
+                return res.status(404).json({ message: 'No user with that ID' });
+            }
+
+            res.status(user);
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    },
+    async removeFriend(req, res) {
+        try {
+            const friend = await User.findOneAndRemove({ _id: req.params.friendId });
+
+            if (!friend) {
+                return res.status(404).json({ message: 'No friend with that id!'})
+            }
+
+            const user = await User.findOneAndUpdate(
+                { friends: req.params.friendId },
+                { $pull: { friends: req.params.friendId }},
+                { new: true }
+            );
+
+            if (!user) {
+                return res.status(404).json({ message: 'No user with that ID '});
+            }
+
+            res.json({ message: 'Friend successfully removed!' });
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    },
+};
