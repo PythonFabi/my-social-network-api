4d84188812ca34c091571a101dbfa7d1f7bfbcdf
PythonFabi <129299589+PythonFabi@users.noreply.github.com>
2023-09-02 14:03:09 -0400

    minor bug fix in the addfriend route

--- controllers/userController.js
+++ controllers/userController.js
@@ -77,7 +77,7 @@ module.exports = {
                 return res.status(404).json({ message: 'No user with that ID' });
             }
 
-            res.status(user);
+            res.json(user);
         } catch (err) {
             res.status(500).json(err);
         }
