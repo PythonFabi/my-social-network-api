4d3a6c7c62fea208d7c9eeed7460372f5fd2b089
PythonFabi <129299589+PythonFabi@users.noreply.github.com>
2023-09-01 21:31:54 -0400

    fix router bug

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -21,3 +21,5 @@ router.route('/:thoughtId/reactions').post(addReaction);
 
 //api/thoughts/:thoughtId/reactions/:reactionId
 router.route('/:thoughtId/reactions/:reactionId').delete(deleteReaction);
+
+module.exports = router;
