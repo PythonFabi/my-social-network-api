577cf6fb264b4221c8e7fc26bede75b16b3f6243
PythonFabi <129299589+PythonFabi@users.noreply.github.com>
2023-09-02 13:11:47 -0400

    display thought model with user model

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,7 +3,8 @@ const { User, Thought } = require('../models');
 module.exports = {
     async getUsers(req, res) {
         try {
-            const users = await User.find();
+            const users = await User.find()
+            .populate('thoughts');
             res.json(users);
         } catch (err) {
             res.status(500).json(err);
